9832c61cd3a473a21ccf9a955b0b76a841215497
cvc360 <dsvfreightforwarding@gmail.com>
2025-04-08 12:31:16 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2444027752"
+    serviceWorkerVersion: "841032287"
   }
 });
